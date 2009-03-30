3d72e8713478b4b407c8ecc50d8f5568cbc6f1db
mincemaker <mincemaker@gmail.com>
2009-03-30 22:11:05 +0900

    add: for python development. add: forgot nav elisp.

--- emacs.d/conf/init-python.el
+++ emacs.d/conf/init-python.el
@@ -1,6 +1,15 @@
-;; Python-mode
+;; pymacs
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+;; python-mode
+(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
+(setq interpreter-mode-alist (cons '("python" . python-mode)
+                                   interpreter-mode-alist))
+(autoload 'python-mode "python-mode" "Python editing mode." t)
 (add-hook 'python-mode-hook
-          '(lambda()
-             (setq indent-tabs-mode nil)
-			 (setq tab-width 4)
-			 ))
+          '(lambda ()
+             (require 'pycomplete)
+             ))
