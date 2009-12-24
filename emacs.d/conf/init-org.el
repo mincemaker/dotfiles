1616d31a8db103d8893908e091a4286f6b131469
mincemaker <mincemaker@gmail.com>
2009-12-24 22:56:45 +0900

    fix: howm-mode setting. fix: org-mode setting.

--- emacs.d/conf/init-org.el
+++ emacs.d/conf/init-org.el
@@ -4,10 +4,14 @@
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (org-remember-insinuate)
 (setq org-directory "~/memo/")
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
 (setq org-default-notes-file (concat org-directory "agenda.org"))
 (setq org-remember-templates
       '(("Todo" ?t "** TODO %?\n   %i\n   %a\n   %t" nil "Inbox")
         ("Bug" ?b "** TODO %?   :bug:\n   %i\n   %a\n   %t" nil "Inbox")
         ("Idea" ?i "** %?\n   %i\n   %a\n   %t" nil "New Ideas")
         ))
+(setq org-log-done t)
+
 
